13fbc5339120d1ac6925d2bcc61117f5233d0370
Joseph Ellis <josephellis95@gmail.com>
2024-04-19 12:17:13 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,63 +1,71 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const md = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+router.get("/", md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
-  res.json('get accounts')    
+    res.json("get accounts");
   } catch (err) {
-      next({status: 422, message: 'this is bad'})
-  }
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('get account')
-  }
-
- catch (err) {
-      next(err)
-  }
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('post account')
-  }
-
- catch (err) {
-      next(err)
-  }
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('update account')
-  }
-
- catch (err) {
-      next(err)
+    next({ status: 422, message: "this is bad" });
   }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get(
+  "/:id",
+  md.checkAccountNameUnique,
+  md.checkAccountId,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("get account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  "/",
+  md.checkAccountNameUnique,
+  md.checkAccountPayload,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("post account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put("/:id", 
+md.checkAccountId, 
+md.checkAccountNameUnique,
+md.checkAccountPayload, 
+(req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('delete account')
+    res.json("update account");
+  } catch (err) {
+    next(err);
   }
+});
 
- catch (err) {
-      next(err)
+router.delete("/:id", md.checkAccountPayload, (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    res.json("delete account");
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // DO YOUR MAGIC
   res.status(err.status || 500).json({
-    message: err.message
-  })
-})
+    message: err.message,
+  });
+});
 
 module.exports = router;
